047f1defcbcc3d40f5641a8c28e0ed0f1e01dbd3
krish <krish.puli11@gmail.com>
2026-05-22 17:36:21 -0400

    assignment2

---
+++ class 2/assignment2.py
@@ -0,0 +1,18 @@
+print("krish")
+print(30)
+print("krish",30, 900.5)
+print("krish", 50.5, 'puli',12,68,90.1)
+print("krish \n"*3)
+print(1+1)
+x=100
+y=50
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+print(x%y)
+number1=30
+number2=31
+sum=number1+number2
+print(sum)
