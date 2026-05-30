9c768fad9bbe42c98087f4f9a52e7c91a2251160
krish <krish.puli11@gmail.com>
2026-05-30 11:30:48 -0400

    marks calculated

---
+++ class4/student marks calculator.py
@@ -0,0 +1,13 @@
+math=int(input("enter math marks:"))
+english=int(input("enter english marks:"))
+science=int(input("enter science marks:"))
+totalmarks=math+english+science
+average=totalmarks/3
+print(f"math marks is {math}")
+print(f"english marks is {english}")
+print(f"science marks is {science}")
+print(f"total marks is {totalmarks}:")
+print(f"average marks is {average}:")
+
+
+            
