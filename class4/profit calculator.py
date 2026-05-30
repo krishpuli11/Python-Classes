08b95ba2122c000f8b6dd9f14e9d7c98d9b6a731
krish <krish.puli11@gmail.com>
2026-05-30 11:04:32 -0400

    class4

---
+++ class4/profit calculator.py
@@ -0,0 +1,7 @@
+sellingprice=int(input("enter selling price:"))
+costprice=int(input("enter cost price:"))
+profit=sellingprice-costprice
+print(f"selling price is {sellingprice}")
+print(f"cost price is {costprice}")
+print(f"profit is {profit}")
+    
