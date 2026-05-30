e5f47f7c45a24d90f787dae5f2a3414f29fae633
krish <krish.puli11@gmail.com>
2026-05-30 10:11:03 -0400

    homework for class 3

---
+++ class3/assignment3.py
@@ -0,0 +1,24 @@
+x=int(input("enter number for x variable:"))
+print(f"data for x entered is {x}")
+y=int(input("enter number for y variable:"))
+print(f"data for y entered is {y}")
+z=x+y
+print(f"sum of x+y={z}")
+
+
+k=int(input("enter number for k variable:"))
+print(f"data for k entered is {k}")
+p=int(input("enter number for p variable:"))
+print(f"data for p entered is {p}")
+z=k*p
+print(f"multiply of k*p={z}")
+
+
+s=int(input("enter number for s variable:"))
+print(f"data for s entered is {s}")
+c=int(input("enter number for c variable:"))
+print(f"data for c entered is {c}")
+div=s/c
+print(f"division of s/c={div}")
+
+
---
+++ class3/datatypes.py
@@ -0,0 +1,10 @@
+x=20
+y=90.5
+name="krishpuli"
+isItRaining=True
+print(f"""
+     value of x is {x}
+type of data stored in x is {type(x)}
+value of y is {y}
+type of data stored in y is {type(y)} 
+      """)
