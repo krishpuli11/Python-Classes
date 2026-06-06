b4cab85edfb0b6445ec8085f2b72ad567e5d9dfd
krish <krish.puli11@gmail.com>
2026-06-06 11:28:40 -0400

    class4 assignment

---
+++ class4/assignment4.py
@@ -0,0 +1,6 @@
+bookprice=int(input("enter price of book:"))
+quantity=int(input("enter number of books:"))
+totalbillamount=quantity*bookprice
+print(f"price of book is {bookprice}")
+print(f"quanity is {quantity}")
+print(f"totalbillamount is {totalbillamount}")
