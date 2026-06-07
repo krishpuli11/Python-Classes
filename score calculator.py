ee05a74d482588eeb6816965d433343e1cbffa39
krish <krish.puli11@gmail.com>
2026-06-07 11:04:15 -0400

    cricket score calculator

---
+++ score calculator.py
@@ -0,0 +1,6 @@
+totalruns=int(input("enter totalruns:"))
+totalmatches=int(input("enter totalmatches:"))
+averagerunspermatch=totalruns/totalmatches
+print(f"averagerunspermatch is{averagerunspermatch}")
+print(f"totalruns is {totalruns}")
+print(f"totalmatches is {totalmatches}")
