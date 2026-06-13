5c9bee530a9664dbfe72ddc57b9e72e725a1f109
krish <krish.puli11@gmail.com>
2026-06-13 11:26:26 -0400

    datatypes

---
+++ class 5/assigment5.py
@@ -0,0 +1,6 @@
+totalschooldays=int(input("enter total school days:"))
+daysattended=int(input("enter days attended:"))
+attendencepercentage=daysattended/totalschooldays*100
+print(f"total school days is {totalschooldays}")
+print(f"days attended is {daysattended}")
+print(f"attendence percentage is {attendencepercentage}")
---
+++ class 5/datatypes.py
@@ -0,0 +1,12 @@
+x=10#int
+print(f"value of x is {x}")
+print(f"type of data is {type(x)}")
+y=20.6#float
+print(f"value of y is {y}")
+print(f"type of data is {type(y)}")
+k="krish"
+print(f"value of k is {k}")
+print(f"type of data is {type(k)}")
+p=True
+print(f"value of p is {p}")
+print(f"type of data is {type(p)}")
