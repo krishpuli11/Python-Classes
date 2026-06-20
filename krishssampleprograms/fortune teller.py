8a21b9fe603fa0a2ba8f46f71391c3e3c3904ab8
krish <krish.puli11@gmail.com>
2026-06-20 03:18:10 -0400

    fortune teller

---
+++ krishssampleprograms/fortune teller.py
@@ -0,0 +1,20 @@
+import random
+
+fortunes=[
+"you will fail your test",
+"you will eat a spider in your sleep",
+"when you go to the airport you will lose your passports and you paid 1600 dollars for the flight -4",
+"as soon as you step out of the door to go out with your friends it starts raining",
+"you will make it to the top two in a Mr.Beast video competing for 500,000 and lose",
+"when the teacher calls on you to answer a question you will fart",
+
+"when you walk outside you will find a 20 dollar bill on the ground ",
+"you will get paid extra at work on pay day",
+"you get 1 wish and you cant wish for more wishes",
+"you got to sleep in for an extra 2 and a half hours",
+"you get a new keyboard and mouse",
+"you are invited to join a Mr.Beast video where you will be competing for 100,000 dollars",
+]
+
+print(random.choice(fortunes))
+
