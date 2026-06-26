1ad6829875c70a7ea98f1e610620ee0ca16d1bf2
krish <krish.puli11@gmail.com>
2026-06-26 10:36:41 -0400

    subtract

---
+++ class 7/basiccalculator.py
@@ -0,0 +1,14 @@
+x=int(input("enter a number:"))
+y=float(input("enter a decimal value:"))
+name=str(input("enter a name:"))
+print(f"x value is {x}")
+print(f"y value is {y}")
+print(f"hello, name")
+sum=x+y
+print(f"sum of {x} and {y} is {sum}")
+
+
+
+
+sub=x-y
+print(f"sub of {x} and {y} is {sub}")
