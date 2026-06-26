3a4ce0e0ad295f062d54ee408bbfa29fed05f181
krish <krish.puli11@gmail.com>
2026-06-26 11:00:23 -0400

    arthemetic operator

--- class 7/basiccalculator.py
+++ class 7/basiccalculator.py
@@ -3,7 +3,7 @@ y=float(input("enter a decimal value:"))
 name=str(input("enter a name:"))
 print(f"x value is {x}")
 print(f"y value is {y}")
-print(f"hello, name")
+print(f"hello, {name}")
 sum=x+y
 print(f"sum of {x} and {y} is {sum}")
 
@@ -15,3 +15,15 @@ print(f"mul of {x} and {y} is {mul}")
 
 div=x/y
 print(f"div of {x} and {y} is {div}")
+
+mod=x%y
+print(f"mod of {x} and {y} is {mod}")
+
+floor=x//y
+print(f"floor div of {x} and {y} is {floor}")
+
+power=x**y
+print(f"power of {x} and {y} is {power}")
+
+
+
