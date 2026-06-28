ea1e340b5539d6843b6b97535bfca0009a71555c
krish <krish.puli11@gmail.com>
2026-06-28 03:30:13 -0400

    assignment2 class7

---
+++ class 7/assignment2.py
@@ -0,0 +1,8 @@
+length=int(input("enter length:"))
+breadth=int(input("enter breadth"))
+area=length*breadth
+perimeter=(length*2)+(breadth*2)
+print(f"length is {length}")
+print(f"breadth is {breadth}")
+print(f"area is {area}")
+print(f"perimeter is {perimeter}")
---
+++ class 7/calenderprinter.py
@@ -0,0 +1,3 @@
+import calendar
+year=int(input("enter any year:"))
+print(calendar.calendar(year))
