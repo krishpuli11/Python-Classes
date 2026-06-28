a13f3c5b8fb4d8ff7e5e498bf11fbe8822f20eb2
krish <krish.puli11@gmail.com>
2026-06-28 08:44:50 -0400

    assignment3 class7

---
+++ class 7/assignment3.py
@@ -0,0 +1,8 @@
+radius=int(input("enter radius:"))
+pi=3.14
+circumference=2*pi*radius
+areaofcircle=pi*(radius*radius)
+print(f"radius is {radius}")
+print(f"pi is {pi}")
+print(f"cicumference is {circumference}")
+print(f"area of circle is {areaofcircle}")
