ff6af04bb7d771dd7dd9bc96820697e615c330db
krish <krish.puli11@gmail.com>
2026-06-28 08:35:36 -0400

    assignment1 class7

---
+++ class 7/assignment1.py
@@ -0,0 +1,12 @@
+number1=int(input("enter number 1:"))
+number2=int(input("enter number 2:"))
+addition=number1+number2
+division=number1/number2
+subtraction=number1-number2
+multiplication=number1*number2
+print(f"number 1 is {number1}")
+print(f"number 2 is {number2}")
+print(f"addition is {addition}")
+print(f"division is {division}")
+print(f"subtraction is {subtraction}")
+print(f"multiplication is {multiplication}")
